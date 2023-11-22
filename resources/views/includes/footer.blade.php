4bfd9ba0245a14bcc1f027b9f1ec232b794bbf20
unknown <ductien96er@gmail.com>
2023-11-22 11:08:44 +0700

    first commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,29 @@
+<footer>
+    <div class="Footer d-flex flex-row col-12">
+        <div class="d-flex flex-column col-4 text-center">
+            <h2>xxiv store</h2>
+            <a href="">Ưu đãi thành viên</a>
+            <a href="">Tài khoản</a>
+            <a href="">Tuyển dụng</a>
+        </div>
+        <div class="d-flex flex-column col-4 text-center">
+            <h2>Chính sách bán hàng</h2>
+            <a href="">Phương thức vận chuyển</a>
+            <a href="">Câu hỏi thường gặp</a>
+            <a href="">Điều khoản và điều kiện sử dụng</a>
+            <a href="">Điều khoản và điều kiện bán hàng</a>
+            <a href="">Thông báo pháp lý</a>
+        </div>
+        <div class="d-flex flex-column col-4 text-center">
+            <h2>Thông tin chung</h2>
+            <a href="">Giới Thiệu</a>
+            <a href="">Blog</a>
+            <a href="">Liên Hệ</a>
+            <a href="">Sản Phẩm</a>
+        </div>
+    </div>
+
+
+
+</footer>
+
