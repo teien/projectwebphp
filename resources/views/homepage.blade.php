2b024d9d707549170d5e75adaeafe0454cabe52f
unknown <ductien96er@gmail.com>
2023-11-22 17:06:37 +0700

    fixhomepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
